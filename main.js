7e362b104a1a399ff199804c8c39b655df45f560
vdtarr <64266312+vdtarr@users.noreply.github.com>
2021-10-08 21:17:13 +0300

    second commit.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   $("#form1").click(function(){
-    $.post("http://localhost:8080/api/add",
+
+  $.post("http://localhost:8080/api/add",
     {
       "name": $("#fname1").val(),
       "email": $("fname2").val()
@@ -10,16 +11,16 @@ $(document).ready(function(){
       console.log("merhaba");
     });
   }); 
+
   $.get("http://localhost:8080/api/all",
   function(data,status){
-    var txt2 = $("<tr><td></td><td></td><td></td></tr>")
+    var txt2 = $("<tr><td class='first'></td><td></td><td></td></tr>")
     data.forEach(elem =>{
-      $("insert").siblings().append(txt2);
-      $("insert").children()[0].children[0].val() = elem.id;
-      $("insert").children()[0].children[1].val() = elem.name;
-      $("insert").children()[0].children[2].val() = elem.email;      
-    })
-  }
-  )
+      $("insert").append(txt2);
+      $("insert").children("tr").children("td.first").val() = elem.id;
+      $("insert").children("tr").children("td.first").next().val() = elem.name;
+      $("insert").children()["tr"].children("td.first").next().next().val() = elem.email;      
+    });
+  });
 
 });
